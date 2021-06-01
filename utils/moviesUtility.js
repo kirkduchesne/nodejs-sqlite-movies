dbf0dc4342a9a5a246949b1231df5ea416855039
Kirk Duchesne <36648041+kirkduchesne@users.noreply.github.com>
2021-06-01 11:34:18 -0400

    uncommented getMovies method

--- utils/moviesUtility.js
+++ utils/moviesUtility.js
@@ -4,7 +4,7 @@ function getMovies() {
     return knex('movies').select('*');
 }
 
-// const getMovies = () => knex('movies').select('*');
+const getMovies = () => knex('movies').select('*');
 
 const getMovie = name => knex('movies').where('title', name).select('movieId');
 
