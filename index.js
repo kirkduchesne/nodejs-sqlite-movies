2a496b76a62fade260142b52d73a6b7ecfaa8a24
Kirk Duchesne <36648041+kirkduchesne@users.noreply.github.com>
2021-06-01 12:40:24 -0400

    moved to sqlite3 tool instead of kenx

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
+const movies = require('./api/routes/movies.js');
 const express = require('express');
-const moviesDB = require('./utils/moviesUtility')
-
 const app = express();
 
 app.use(express.json());
+
+app.use('/movies', movies);
 
 app.get('/', (req, res) => {
     res.send('Server is active.');
 });
 
-app.get('/movies', async (req, res) => {
-    const movies = await moviesDB.getMovies();
-    res.status(200).json({movies});
-});
-
-app.get('/:name', async (req, res) => {
-    const movie = await moviesDB.getMovie(req.params.name);
-    res.status(200).json({movie});
+app.get('/movielist', (req, res) => {
+    db.all("SELECT Title FROM movies LIMIT 5", (err, rows) => {
+        if (err) {
+            return console.error(err.message);
+        }
+        if (rows) {
+            rows.forEach((row) => {
+                console.log(row.title);
+            })
+            res.json({rows});
+        } else if (!rows) {
+            res.send('No movie titles have been found.');
+        }
+    });
 });
 
 const PORT = 5000 || process.env.PORT;
@@ -24,3 +31,13 @@ const PORT = 5000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Listening on PORT: ${PORT}`);
 });
+
+/* Unable to resolve kenx issues */
+// Kenx provides better organization like shown below
+
+// const moviesDB = require('./utils/moviesUtility')
+
+/* app.get('/:name', async (req, res) => {
+    const movie = await moviesDB.getMovie(req.params.name);
+    res.status(200).json({movie});
+}); */
